cea545002096fbd8ff0b7bd4afbf23711ef01b28
katman-Generation <tkatema5@gmail.com>
2024-01-03 19:54:23 +0200

    adding account creation page

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.contrib import messages
+from django.contrib.auth.models import User, auth
 from .models import Post
 from .forms import PostForm
+
 
 def post_list(request):
     """for  the view of the home page"""
@@ -58,3 +61,39 @@ def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect('post_list')
+
+def create_account(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email taken')
+                return redirect('create_account')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Taken')
+                return redirect('create_account')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+                
+                # log user in and redirect to setting page
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
+                
+                # create a profile object for the new user
+                #user_model = User.objects.get(username=username)
+                #new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                #new_profile.save()
+                return redirect('post_list')
+                
+        else:
+            messages.info(request, 'password not matching')
+            return redirect('create_account')
+            
+        
+    else:
+        return render(request, 'registration/create.html')
